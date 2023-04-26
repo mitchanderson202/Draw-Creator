c2c87c4392579b08108cae6267c0cce5a271ac63
Mitchell Anderson <mitchellanderson202@hotmail.com>
2023-04-26 11:25:25 +1000

    updated Welcome instructions

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -4,7 +4,42 @@ const Welcome = () => {
   return (
     <div className="Welcome">
       <h1>Draw Creator</h1>
-      <p>Enter team names and submit. add instructions</p>
+      <p>
+        Welcome to Draw Creator, enter a bunch of team names. (if uneven, that's
+        ok there will be a "BYE). You must enter last years Finalists as they
+        wil open the season in Round 1. Select dates and once you the finalise a
+        random draw will be created.
+        <br />
+        Currently, there is a randomiser algorithm in place. There is nothing
+        making sure teams dont play eachother several weeks in a row, or an even
+        amount of times compared to other teams. Stay tuned for updates.
+        <br />
+        If you want to see more click{" "}
+        <a
+          href="https://github.com/mitchanderson202"
+          target="_blank"
+          rel="noopener"
+        >
+          HERE FOR GITHUB
+        </a>{" "}
+        ||{" "}
+        <a
+          href="https://www.linkedin.com/in/mitchandersondeveloper/"
+          target="_blank"
+          rel="noopener"
+        >
+          HERE FOR LINKEDIN
+        </a>{" "}
+        ||{" "}
+        <a
+          href="https://mitchandersondeveloper.com/"
+          target="_blank"
+          rel="noopener"
+        >
+          HERE FOR WEBSITE
+        </a>
+        .
+      </p>
     </div>
   );
 };
